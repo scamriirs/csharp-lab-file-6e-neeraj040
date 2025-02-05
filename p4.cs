c523c7d483a77f8b2683761cc2b069f023ee7fc9
neeraj040 <163624439+neeraj040@users.noreply.github.com>
2025-02-05 12:10:16 +0530

    Create p4.cs
    Write a program  to show usage of this keyword

---
+++ p4.cs
@@ -0,0 +1,49 @@
+//NEERAJ-(AI&ML)020
+
+using System;
+
+namespace ThisKeywordDemo
+{
+    class Student
+    {
+        private string name;
+        private int age;
+
+       
+        public Student(string name, int age)
+        {
+            this.name = name; 
+            this.age = age;
+        }
+
+        
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Name: {this.name}, Age: {this.age}");
+        }
+
+        
+        public Student() : this("Default Name", 18)
+        {
+            Console.WriteLine("Default constructor called!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Student student1 = new Student("Alice", 20);
+            student1.DisplayDetails();
+
+            Console.WriteLine();
+
+          
+            Student student2 = new Student();
+            student2.DisplayDetails();
+
+            Console.ReadLine();
+        }
+    }
+}
